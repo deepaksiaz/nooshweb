99d6dad1ec0c6ef9e057c63a003f956ed979ac95
deepaksiaz <101709977+deepaksiaz@users.noreply.github.com>
2022-06-14 16:18:48 +0530

    final

---
+++ src/components/navigation/navigation.js
@@ -0,0 +1,15 @@
+import React from "react";
+import{ FcNext,FcPrevious }from "react-icons/fc"
+import "./navigation.scss";
+import img from "../../asset/page-1.png"
+import img1 from "../../asset/page.png"
+import { Helmet } from "react-helmet";
+const Navigation = () => {
+  return (
+    <>
+  
+ 
+   </>
+  );
+};
+export default Navigation;
