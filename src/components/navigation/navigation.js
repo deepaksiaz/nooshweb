1455f6df4c1d72162ee5c23bed5f2becbc552131
deepaksiaz <101709977+deepaksiaz@users.noreply.github.com>
2022-07-04 15:44:27 +0530

    UI

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./navigation.scss";
+import logo from "../../asset/logo noosh.png"
 const Navigation = () => {
   return (
     <>
-  <nav class="navbar navbar-light navbar-expand-lg" style={{backgroundColor: "#e3f2fd"}}>
+  <nav class="navbar navbar-light navbar-expand-lg nav-bar-2" >
             <div class="container-fluid">
               <a class="navbar-brand me-2 nav-link-1" href="/">
-                NOOSH
+              <img src={logo} alt="" className="img-fluid-logo"/>
               </a>
               <button
-                class="navbar-toggler"
+                className="navbar-toggler toggled"
                 type="button"
                 data-mdb-toggle="collapse"
                 data-mdb-target="#navbarButtonsExample"
@@ -20,23 +21,23 @@ const Navigation = () => {
                 <i class="fas fa-bars"></i>
               </button>
 
-              <div class="collapse navbar-collapse" id="navbarButtonsExample">
+              <div class="collapse navbar-collapse nav-option" id="navbarButtonsExample">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
 
                 <div class="d-flex align-items-center">
                   <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                      <a class="navbar-brand me-2 nav-link-2" href="/">
+                      <a class="me-3 nav-link-2" href="/">
                         HOME
                       </a>
                     </li>
                     <li class="nav-item">
-                      <a class="navbar-brand me-2 nav-link-2" href="/">
+                      <a class="me-3 nav-link-2" href="/">
                         ABOUT US
                       </a>
                     </li>
                     <li class="nav-item">
-                      <a class="navbar-brand me-2 nav-link-2" href="/contactus">
+                      <a class="me-2 nav-link-2" href="/contactus">
                         CONTACT US
                       </a>
                     </li>
